aa1f83337849aa87e27bfbf567d9c40f58aeb7c2
qawsed281113 <qawsed281113@users.noreply.github.com>
2020-12-06 21:42:48 +0200

    added view responce to php

---
+++ application/views/templates/viewResponce.php
@@ -0,0 +1,40 @@
+<!-- 
+   $respText => responce text ;
+   $userName => user name ;
+ 
+
+
+ -->
+
+
+
+
+
+
+<div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 class=""><?=$userName?></h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12" style=""><a class="btn btn-lg btn-light" href="#"><i class="fa fa-star fa-fw fa-1x py-1"></i></a>
+        <a class="btn btn-lg btn-light" href="#"><i class="fa fa-star fa-fw fa-1x py-1"></i>
+    </a><a class="btn btn-lg btn-light" href="#"><i class="fa fa-star fa-fw fa-1x py-1"></i>
+</a><a class="btn btn-lg btn-light" href="#"><i class="fa fa-star fa-fw fa-1x py-1"></i></a>
+<a class="btn btn-lg btn-light" href="#"><i class="fa fa-star fa-fw fa-1x py-1"></i></a>
+</div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <p class="mt-3 mb-3">  <?=$respText?>   </p> 
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+        </div>
+      </div>
+    </div>
+  </div>
+ 
