7a223ef2588cc6f457150e0b764772c37b0d5227
qawsed281113 <qawsed281113@users.noreply.github.com>
2020-12-06 22:01:39 +0200

    Update login.php

--- application/views/templates/login.php
+++ application/views/templates/login.php
@@ -16,7 +16,7 @@
                 <input type="password" class="form-control w-75" id="inputpasswordh" placeholder="Password"> </div>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
-            <a href="#" class="p-2" style="/login">Sign in</a>
+            <a href="#" class="p-2" style="/register">Sign in</a>
           </form>
         </div>
        
