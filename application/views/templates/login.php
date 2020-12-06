62e214c449667541b4f1831f7659bf9d782ea22b
qawsed281113 <qawsed281113@users.noreply.github.com>
2020-12-06 21:49:47 +0200

    login to php

---
+++ application/views/templates/login.php
@@ -0,0 +1,25 @@
+<body class="h-100">
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12"><img class="img-fluid d-block mx-auto mb-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/505px-Apple_logo_black.svg.png" width="50"></div>
+      </div>
+      <div class="row mx-5">
+        <div class="col-md-12 pl-5" style="">
+          <form id="c_form-h" class="ml-5 pl-5">
+            <div class="form-group row"> <label for="inputmailh" class="col-2 col-form-label">User Name</label>
+              <div class="col-10">
+                <input type="text" class="form-control w-75" id="inputmailh" placeholder="User"> </div>
+            </div>
+            <div class="form-group row"> <label for="inputpasswordh" class="col-2 col-form-label">Password</label>
+              <div class="col-10">
+                <input type="password" class="form-control w-75" id="inputpasswordh" placeholder="Password"> </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <a href="#" class="p-2" style="">Sign in</a>
+          </form>
+        </div>
+       
+      </div>
+    </div>
+  </div>
