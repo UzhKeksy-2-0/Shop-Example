391977b75a74650efa479b4fae469d2295bc39b8
qawsed281113 <qawsed281113@users.noreply.github.com>
2020-12-06 21:55:52 +0200

    todo

---
+++ application/controllers/Login.php
@@ -0,0 +1,24 @@
+<?php
+
+use app\models\Product;
+use UK\UK_Controller;
+use UK\UK_Log;
+
+class HomDatae extends UK_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function pageLoad(){
+        Product::connect('localhost','root','','flower_shop');
+        $products = Product::get();
+        $this->load($this->views_file->templates->header,[]);
+        $this->load($this->views_file->templates->login,[]);
+        $this->load($this->views_file->templates->footer,[]);
+
+
+
+        
+    }
+}
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -21,7 +21,7 @@
         </ul>
         <ul class="navbar-nav">
           <li class="nav-item"> <a class="nav-link" href="/login">Log in</a> </li>
-          <li class="nav-item"> <a class="nav-link text-primary" href="#">Register</a> </li>
+          <li class="nav-item"> <a class="nav-link text-primary" href="/register">Register</a> </li>
         </ul>
       </div>
     </div>
