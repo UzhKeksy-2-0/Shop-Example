b0e1030b43f7889a4c210d47fdcc9e0be8a839db
XBaTuT <69810357+XBaTuT@users.noreply.github.com>
2020-12-08 10:39:22 +0200

    Update order_status.php

---
+++ application/models/order_status.php
@@ -0,0 +1,44 @@
+<?php
+    namespace app\models;
+
+use UK\UK_DataBase;
+
+class order_status extends UK_DataBase
+{
+    //protected $dbTable = "order_status";
+    public $id;
+    public $name;
+
+    /**
+    * Get $id
+    */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+    * Get $name
+    */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+    * Set $id
+    */
+    public function setId( $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+    * Set $name
+    */
+    public function setName( $name)
+    {
+        $this->name = $name;
+    }
+
+}
