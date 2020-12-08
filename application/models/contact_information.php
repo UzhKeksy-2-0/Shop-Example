8427c878e9eb359b8776ef0c9dd2f0c1bb5e8ce6
XBaTuT <69810357+XBaTuT@users.noreply.github.com>
2020-12-08 10:04:59 +0200

    Update contact_information.php

---
+++ application/models/contact_information.php
@@ -0,0 +1,78 @@
+<?php
+    namespace app\models;
+
+use UK\UK_DataBase;
+
+class contact_information extends UK_DataBase
+{
+    //protected $dbTable = "contact_information";
+    public $id;
+    public $user_id;
+    public $contact_type_id;
+    public $contact;
+
+    /**
+    * Get $id
+    */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+    * Get $user
+    */
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    /**
+    * Get $contacttypeid
+    */
+    public function getContact_type_id()
+    {
+        return $this->contact_type_id;
+    }
+
+    /**
+    * Get $contact
+    */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+
+    /**
+    * Set $id
+    */
+    public function setId( $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+    * Set $userid
+    */
+    public function setUser_id( $user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+    * Set $contacttypeid
+    */
+    public function setContact_type_id( $contact_type_id)
+    {
+        $this->contact_type_id = $contact_type_id;
+    }
+
+    /**
+    * Set $contact
+    */
+    public function setContact( $contact)
+    {
+        $this->contact = $contact;
+    }
+
+}
