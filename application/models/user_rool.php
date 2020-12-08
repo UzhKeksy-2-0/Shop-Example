c2f6ca0af722ebbe4c709394a85205342eba4043
XBaTuT <69810357+XBaTuT@users.noreply.github.com>
2020-12-08 11:48:34 +0200

    Create user_rool.php

---
+++ application/models/user_rool.php
@@ -0,0 +1,44 @@
+<?php
+    namespace app\models;
+
+use UK\UK_DataBase;
+
+class user_rool extends UK_DataBase
+{
+    //protected $dbTable = "user_rool";
+    public $id;
+    public $name;
+
+    /**
+    * Get $id
+    */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+    * Get $name
+    */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+    * Set $id
+    */
+    public function setId( $id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+    * Set $name
+    */
+    public function setName( $name)
+    {
+        $this->name = $name;
+    }
+
+}
