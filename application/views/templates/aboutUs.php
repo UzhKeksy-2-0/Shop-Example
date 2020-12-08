d6e011863e2208fc0cf5ba375277106696cd663d
qawsed281113 <qawsed281113@users.noreply.github.com>
2020-12-08 20:45:55 +0200

    Add to Header link "About Us" and "Contacts"

---
+++ application/views/templates/aboutUs.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.3.1.css">
+</head>
+
+<body>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="px-md-5 p-3 col-md-6 d-flex flex-column align-items-start justify-content-center">
+          <h1>About Us</h1>
+          <p class="mb-3 lead" >Has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+        </div>
+        <div class="col-md-6"><img class="img-fluid d-block" src="https://duck.show/wp-content/uploads/2019/09/2019-09-17_17-11-17.png"></div>
+      </div>
+    </div>
+  </div>
+  <li class="nav-item"> <a class="nav-link text-primary" href="/">About us</a> </li>
+</body>
+
+</html>
