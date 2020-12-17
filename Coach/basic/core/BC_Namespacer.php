27dd36cf16ad4bc182f275f4788869c6ece5185b
Duvi-Gazda <gazda.haimdavid@gmail.com>
2020-12-18 01:36:41 +0200

    add namespace generator

--- Coach/basic/core/BC_Namespacer.php
+++ Coach/basic/core/BC_Namespacer.php
@@ -10,34 +10,7 @@ use CH\modificators\Log\CH_Log;
  */
 class BC_Namespacer
 {
-    private $namespaces;    
-    private $parentNamespace;
-    public function __construct()
-    {
-        /* if were given more arguments call other constructoe that is named 
-        as constructor with num of arguments htat was given*/
-        $arguments = func_get_args();
-        $numberOfArguments = func_num_args();
-        if (method_exists($this, $function = '__construct' . $numberOfArguments)) 
-        {
-            call_user_func_array(array($this, $function), $arguments);
-        } else 
-        {
-            // get data from file. But we have to take all namespaces and parentNamespaces
-            $fileData = json_decode(file_get_contents(__DIR__.'/namespaces.json'),true);
-
-        }
-    }    
-    /**
-     * __construct1
-     * creates configs by cinfig file
-     * @param  mixed $configFile
-     * @return void
-     */
-    public function __construct1($configFile)
-    {
-        $this->namespaces = json_decode(file_get_contents($configFile),true);
-    }    
+    public static string $composerPath;
     /**
      * createConfigFile
      * creates config file by composer. gets only parents namespaces
@@ -45,76 +18,57 @@ class BC_Namespacer
      * @param  string $result
      * @return array asoc all parent configs
      */
-    public static function createConfigFile(string $composerPath, string $result = './namespaces.json')
+    public static function createConfigFile(string $result = './namespaces.json')
     {
-        // get data from composer. We do it to take all parent namespaces that are static
-        $log = new CH_Log;
-        $configs = json_decode(file_get_contents($composerPath),true);
+        $configs = self::getParentNameSpaces();
+        file_put_contents($result,json_encode($configs));
+        return $configs;
+    }        
+    /**
+     * getParentNameSpaces
+     * returns parent namespaces using composer
+     * IMPORTANT static value $composerPath has to be initilazed !
+     * @return array
+     */
+    public static function getParentNameSpaces(): array{
+        $configs = json_decode(file_get_contents(self::$composerPath),true);
         // get only neccesary data
         $configs = $configs['autoload']['psr-4'];
         foreach($configs as $key => $value){
             unset($configs[$key]);
-            // $value = stripslashes($value);
-            $configs[stripslashes($key)] =$value;
+            $configs[stripslashes($key)] = $value;
         }
         foreach($configs as $key => $value){
-            $configs[$key] =explode('/',$value)[0];
-            // $configs[$key] =str_replace("\\",'',$value);
+            $configs[$key] = explode('/',$value)[0];
         }
-        //$configs = stripslashes($configs);
-        //  write complete data to file
-        file_put_contents($result,json_encode($configs));
         return $configs;
     }    
     /**
-     * addNameSpace
-     * only add namespace
-     * @param  mixed $namespace
+     * generateNameSpaceByPath
+     * generates and returns namespace according to given path
+     * @param  mixed $path
      * @return void
      */
-    public function addNameSpaceByName(string $namespaceName)
-    {
-        $namespaces = explode("\\",$namespaceName);
-        array_push($this->namespaces,$namespaces);
-    }
-
-
-    /**
-     * Get the value of namespaces
-     */ 
-    public function getNamespaces()
-    {
-        return $this->namespaces;
-    }
-
-    /**
-     * Set the value of namespaces
-     *
-     * @return  self
-     */ 
-    public function setNamespaces($namespaces)
-    {
-        $this->namespaces = $namespaces;
-        return $this;
-    }
-
-    /**
-     * Get the value of parentNamespace
-     */ 
-    public function getParentNamespace()
-    {
-        return $this->parentNamespace;
-    }
-
-    /**
-     * Set the value of parentNamespace
-     *
-     * @return  self
-     */ 
-    public function setParentNamespace($parentNamespace)
-    {
-        $this->parentNamespace = $parentNamespace;
-        return $this;
+    public static function generateNameSpaceByPath(string $path){
+        $parentNamespaces = self::getParentNameSpaces($path);
+        $path = explode('/',$path);
+        $result =  '';
+        $number = null;
+        foreach($path as $num => $subPath){
+            foreach($parentNamespaces as $parentNamespace){
+                if($subPath == $parentNamespace){
+                    $number = $num;
+                    $result = $subPath;
+                }
+            }
+        }
+        if($result == ''){
+            return;
+        } else {
+            for($i = $number; $i < count($path); ++$i){
+                $result .= $path[$i];
+            }
+        }
+        return $result;
     }
 }
-
--- public/tester.php
+++ public/tester.php
@@ -8,3 +8,13 @@ $data = new Namespacer();
 $log = new CH_Log;
 // $log->log($data->getNamespaces(),null , true);
 $log->log($data->createConfigFile('../composer.json'),null ,true);
+$arry1 = [
+    'home' => 'data'
+];
+$home = [
+    'home'=> [
+        'data' => 'wow'
+    ]
+];
+array_push($arry1,$home);
+$log->log($arry1, null, true);
