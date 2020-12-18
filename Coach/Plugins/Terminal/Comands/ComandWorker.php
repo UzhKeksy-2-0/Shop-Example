978d9061ae0efa5051be11283746309f9cdf7034
Duvi-Gazda <gazda.haimdavid@gmail.com>
2020-12-18 03:11:47 +0200

    add function delete comand

--- Coach/Plugins/Terminal/Comands/ComandWorker.php
+++ Coach/Plugins/Terminal/Comands/ComandWorker.php
@@ -3,6 +3,7 @@ namespace CH\Plugins\Terminal\Comands;
 
 use CH\modificators\Terminal\Terminal;
 use CH\basic\core\BC_Namespacer;
+use CH\modificators\File\FileWorker\FileWorker;
 use CH\Plugins\File\P_FileWorker;
 use Nette\PhpGenerator\PhpFile;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,23 +14,28 @@ use Symfony\Component\Console\Question\Question;
 
 class ComandWorker extends Terminal
 {
+    private string $root;
     protected static $defaultName = 'terminal';
-    /**
-     * configure
-     *
-     * @return void
-     */
+    public function __construct(string $root)
+    {
+        $this->root = $root;
+        parent::__construct();
+    }
     protected function configure()
     {
-        $this
-            ->setHelp('test')
-            ->setDescription('-c:d creates by default (no help no description no argments)');
         $this
             ->addOption(
                 'create',
                 '-c',
                 InputOption::VALUE_OPTIONAL,
-                'create new command',
+                "create new command \n<info>Values</info>   default (d);   min;   max",
+                null,
+            )
+            ->addOption(
+                'remove',
+                '-r',
+                InputOption::VALUE_OPTIONAL,
+                "removes comand by name (Name have to be letter in letter)",
                 null
             );
     
@@ -37,7 +43,11 @@ class ComandWorker extends Terminal
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln($input->getOption('create'));
+        // $output->writeln($input->getOption('create'));
+        if($input->getOption('remove') != null){
+            $output->writeln($this->root .'/'. $input->getOption('remove'). '.php');
+            (new FileWorker)->remove($this->root .'/'. $input->getOption('remove'). '.php');
+        }
         return Terminal::SUCCESS;
     }
 }
